ced17107f6a9d86b551eaa6366511af9c77bf9d0
thedumbinventor <priyanshu.electronics@sunfox.in>
2023-02-13 16:44:44 +0530

    Added hashmap

---
+++ src/StudentHash.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class StudentHash {
+    public static void main(String[] args) {
+        Map<String, Integer> studentMap = new HashMap<>();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("EnterDetails");
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            if (line.isEmpty()) {
+                break;
+            }
+            String[] parts = line.split(" ");
+            String name = parts[0];
+            int studentId = Integer.parseInt(parts[1]);
+            studentMap.put(name, studentId);
+        }
+        System.out.println("Information");
+        for (Map.Entry<String, Integer> entry : studentMap.entrySet()) {
+            System.out.println("Name: " + entry.getKey() + ", ID: " + entry.getValue());
+        }
+    }
+}
